8f91f6e9426ee7a7b77899e8364de721e82e1cd9
THANUJ KUMAR S C <thanuj_tk@yahoo.com>
2018-12-17 21:46:00 +0530

    viewresolver and content negotiation examples

---
+++ src/main/java/com/app/mvc/Cat.java
@@ -0,0 +1,51 @@
+package com.app.mvc;
+
+public class Cat {
+
+	private String name;
+	private String breed;
+	private int age;
+	private String tip = "This domain object provided by ContentNegotiatingViewResolver";
+	
+	public Cat(String name, String breed, int age) {
+		this.name = name;
+		this.age = age;
+		this.breed = breed;
+				
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getTip() {
+		return tip;
+	}
+
+	public void setTip(String tip) {
+		this.tip = tip;
+	}
+	
+	
+
+}
--- src/main/java/com/app/mvc/SpringBootMvcApplication.java
+++ src/main/java/com/app/mvc/SpringBootMvcApplication.java
@@ -2,10 +2,25 @@ package com.app.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class SpringBootMvcApplication {
 
+	@RequestMapping("/viewresolver")
+	public String viewResolver() {
+		return "viewresolver";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/cat")
+	public Cat cat() {
+		return new Cat("Athena","Bengal",1);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcApplication.class, args);
 	}
